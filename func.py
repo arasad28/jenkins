a2ba327c26cb1dd28c84252c261743f0abfe4388
arasad28 <asadfe24@gmail.com>
2023-11-04 15:28:05 +0600

    fist commit

---
+++ func.py
@@ -0,0 +1,13 @@
+def add(x,y):
+    return x+y
+def subtract(x,y):
+    return x-y
+def multi(x,y):
+    return x*y
+def divide(x,y):
+    return x/y
+def modulo(x,y):
+    return x%y
+def exp(x,y):
+    return x**y
+
---
+++ test.py
@@ -0,0 +1,14 @@
+from func import *
+
+def test_add():
+    assert add(3,4) == 7
+def test_substract():
+    assert subtract(7,10) == -3
+def test_multi():
+    assert multi(2,3) == 6
+def test_divide():
+    assert divide(5,4) == 1
+def test_modulo():
+    assert modulo(4,3) == 1
+def test_exp():
+    assert exp(3,4) == 81
